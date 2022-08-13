8267f96ab71a909e7ae02609ffd22caece4a0b28
kuro <kuro@jiayeli.cn>
2022-08-13 23:19:46 +0800

    [add] webpalteform module  init and add bootstrap package

---
+++ webPlatform/src/test/java/ST.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ST {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+    }
+
+}
